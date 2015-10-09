85f610c107a45473231a5a5809cb99442fd861f1
Graphic <graphic@SOD-SJH-516-08M.local>
2015-10-09 12:08:41 -0400

    switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 
+var switchon=true
 
+if($('.body').hasClass('light') && switchon=true){
+	$('.switch').on('click', function(e){
+	$('.status').text('Hey, who turn off the lights?')
+	$('body').removeClass('light')
+	$('body').addClass('dark')
+	})}
+
+
+else if($('.body').hasClass('dark')){
+	$('.switch').on('click', function(e){
+	$('.status').text("It's so bright in here!")
+	$('body').removeClass('dark')
+	$('body').addClass('light')
+
+	
+
+})}
